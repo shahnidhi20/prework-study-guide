47dbffda70f8d7f513afb82f6ff4b1e4ad512189
Nidhi Shah <nidhi@Nidhis-MacBook-Pro.local>
2023-03-12 10:18:29 -0700

    Added javascript

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+
+/*
+var topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }*/
+
+  var topics = ["HTML", "CSS", "GIT", "Javascript"];
+
+  for(var i = 0; i < topics.length ; i++){
+    console.log(topics[i]);
+  }
